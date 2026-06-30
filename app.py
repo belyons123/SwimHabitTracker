229bc1bd17125664c3bfba207c0a810504c12a4e
Benjamin Lyons <140098106+belyons123@users.noreply.github.com>
2026-06-29 22:57:56 -0400

    Added login & register routes/html forms. Also did a bare home page.

--- app.py
+++ app.py
@@ -3,15 +3,19 @@
 #Date created: 6/27/2026
 
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+from models.user import create_user, verify_user
 
 #Database establishment
-connection = sqlite3.connect('dabase.db')
+connection = sqlite3.connect("database.db")
 cursor = connection.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL UNIQUE
+        username TEXT NOT NULL UNIQUE,
+        hashed_password TEXT NOT NULL,
+        salt TEXT NOT NULL,
+        created_at TEXT NOT NULL
     )
 ''')
 
@@ -19,10 +23,38 @@ cursor.execute('''
 
 #Create the Flask application
 app = Flask(__name__)
-#- Routes
+app.secret_key = "chewie"
+
+#- Route 1: Register
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        create_user(username, password)
+        return redirect("/login")
+
+    return render_template("register.html")
+
 @app.route("/")
 def home():
     return "<h1>Swimmer Habit Tracker</h1><p>Your app is running!</p>"
+#- Route 2: Login
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user_id = verify_user(username,password)
+        if user_id:
+            return redirect("/home")
+        else:
+            return render_template("login.html")
+#- temporary home page
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
 
 #*Later, I'll replace this with:
 # return render_template("dashboard.html")
