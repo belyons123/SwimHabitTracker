1775828375fc73c71a169fdab49fb16f72b4c4d1
Benjamin Lyons <140098106+belyons123@users.noreply.github.com>
2026-08-29 00:36:08 -0400

    Added  video_detail. & video_library .html files. update base for buttons and app for routing.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 import sqlite3
 from flask import Flask, render_template, request, redirect, session
 from models.user import create_user, verify_user
+from models.videos import get_all_videos, get_video
 
 #Database establishment
 connection = sqlite3.connect("database.db")
@@ -38,7 +39,7 @@ app.secret_key = "chewie"
 @app.route("/") #Root where people will first vist, currently only goes to register for test purposes
 def root():
     return redirect("/register")
-#- Route 1: Register
+#Register
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -52,7 +53,7 @@ def register():
         return redirect("/login")
     return render_template("register.html", error=None)
 
-#- Route 2: Login
+#Login
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method == "POST":
@@ -68,10 +69,30 @@ def login():
             return render_template("login.html", error="Invalid username or password.")
     return render_template("login.html", error=None)
 
-#- temporary home page
+#Home Page
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+#LargerVideo Library
+@app.route("/videos")
+def video_library():
+    if "user_id" not in session:
+        return redirect("/login")
+
+    stroke = request.args.get("stroke")  # optional filter
+    videos = get_all_videos(stroke)
+
+    return render_template("video_library.html", videos=videos)
+
+#Detail Video
+@app.route("/videos/<int:video_id>")
+def video_detail(video_id):
+    if "user_id" not in session:
+        return redirect("/login")
+
+    video = get_video(video_id)
+    return render_template("video_detail.html", video=video)
 
 #- Run the app
 if __name__ == "__main__":
